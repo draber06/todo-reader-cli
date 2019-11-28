16bb0a97d4d4e767419defd224f97b481c5911ac
Alexey Eremin <draber06@gmail.com>
2019-11-28 16:29:53 +0300

    Add sort commands

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
 const { readLine } = require('./console');
 const path = require('path');
 
-const store = [];
+const toDosStore = [];
 app();
 
 function app() {
@@ -11,7 +11,7 @@ function app() {
     console.log('Please, write your command!');
 
     const toDos = extractToDos(files);
-    store.push(...toDos);
+    toDosStore.push(...toDos);
 
     readLine(processCommand);
 }
@@ -29,7 +29,7 @@ function extractToDos(files) {
             const todoProps = todo.split(';').map((prop) => prop.trim());
             if (todoProps.length === 1) {
                 const [comment] = todoProps;
-                return { comment, filename, user: null, date: null };
+                return { user: '', date: '', comment, filename };
             }
             const [user, date, comment] = todoProps;
             return { user: user.toLowerCase(), date, comment, filename };
@@ -52,24 +52,76 @@ function processCommand(command) {
             process.exit(0);
             break;
         case 'show':
-            console.log(store);
+            console.log(toDosStore);
             break;
         case 'important':
-            const importantToDos = store.filter(({ comment }) =>
+            const importantToDos = toDosStore.filter(({ comment }) =>
                 comment.includes('!')
             );
             console.log(importantToDos);
             break;
         case 'user':
-            const userToDos = store.filter(
+            const userToDos = toDosStore.filter(
                 ({ user }) => user === option.toLowerCase()
             );
             console.log(userToDos);
+            break;
+        case 'sort':
+            const sortedToDos = sortToDos(option);
+            console.log(sortedToDos);
             break;
         default:
             console.log('wrong command');
             break;
     }
+}
+
+function sortToDos(order) {
+    const mapOrderToSort = {
+        importance: sortByImportance,
+        user: sortByUser,
+        date: sortByDate
+    };
+
+    return mapOrderToSort[order](toDosStore);
+}
+
+function sortByImportance(coll) {
+    return [...coll].sort(
+        (a, b) =>
+            countRepeatedChars(b.comment, '!') -
+            countRepeatedChars(a.comment, '!')
+    );
+}
+
+function sortByUser(coll) {
+    return [...coll].sort((a, b) => {
+        let firstLetterA = a.user[0] || 'z';
+        let firstLetterB = b.user[0] || 'z';
+
+        if (firstLetterA > firstLetterB) {
+            return 1;
+        }
+        if (firstLetterA < firstLetterB) {
+            return -1;
+        }
+
+        return 0;
+    });
+}
+
+function sortByDate(coll) {
+    return [...coll].sort((a, b) => {
+        const dataA = Date.parse(a.date) || Infinity;
+        const dataB = Date.parse(b.date) || Infinity;
+
+        return dataA - dataB;
+    });
+}
+
+function countRepeatedChars(string, char) {
+    const pattern = new RegExp(`[^${char}]`, 'g');
+    return string.replace(pattern, '').length;
 }
 
 // TODO you can do it!
